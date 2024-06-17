ecca9ec950337e2f47090713cdba4277adb9df9e
ranan <ranan@email.com>
2024-06-17 14:21:08 +0800

    feat: 加入vite搭建的play包，用于测试vue组件

---
+++ play/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Error class="icon" />
+  <Success class="icon" />
+</template>
+
+<script setup lang="ts">
+import { Error, Success } from '@capybara-ui/icons-vue'
+</script>
+
+<style scoped>
+.icon {
+  height: 48px;
+}
+</style>
